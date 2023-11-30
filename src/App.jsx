dc7782e11b8bb23e1695355944add8c2315aa6e4
Victorer <rainishere20@gmail.com>
2023-11-30 13:39:47 +0900

    auto numbering

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import "@fontsource/roboto"; // Defaults to weight 400.
 import Link from './Link.jsx';
 import logo from '/rainishere_s.png'
 import './fonts.css'
+import data from './Data.json';
 
 
 function App() {
@@ -21,7 +22,7 @@ function App() {
         <Link />
         
       </div>
-      <p className="read-the-docs">以上的11個連結是RainIsHere的真實社交平台及獨家優惠連結，其他沒有登記的平台及連結全部屬於虛假帳戶或假平台，請大家小心騙徒喔！</p>
+      <p className="read-the-docs">以上的{data.length}個連結是RainIsHere的真實社交平台及獨家優惠連結，其他沒有登記的平台及連結全部屬於虛假帳戶或假平台，請大家小心騙徒喔！</p>
     </>
   )
 }
