c0d8413e87f301e2eb64d51b491859f6a19791df
Victorer <rainishere20@gmail.com>
2023-12-22 13:19:48 +0900

    change to hash router

--- src/blog.jsx
+++ src/blog.jsx
@@ -1,11 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { useLocation, useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import './blog.css';
 import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 import blog from './blog.json';
-// import {markdown} from './blogContents';
-
 
 function Blog() {
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,36 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import Blog from './blog.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import Blog from "./blog.jsx";
+import "./index.css";
 // import backgroundVideo from '/background.webm'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-  },
-  {
-    path: "/blog/:folder",
-    element: <Blog/>
-  }
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App/>,
+//   },
+//   {
+//     path: "/blog/:folder",
+//     element: <Blog/>
+//   }
+// ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// const router = createHashRouter();
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     {/* <div class="video-background">
   <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
       <source src={backgroundVideo} type="video/webm"/>
   </video>
 </div> */}
-  <RouterProvider router={router} />
-  </React.StrictMode>,
-)
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/blog/:folder" element={<Blog />} />
+      </Routes>
+    </HashRouter>
+  </React.StrictMode>
+);
