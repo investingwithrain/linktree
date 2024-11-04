6120320a550639cf1f31a848e3276d8c775ffac2
Victorer <rainishere20@gmail.com>
2024-11-04 16:16:07 +0000

    auto push

--- src/Link.jsx
+++ src/Link.jsx
@@ -3,14 +3,29 @@ import "@fontsource/roboto"; // Defaults to weight 400.
 import './Link.css'
 import './fonts.css'
 
-function Link({ data}) {
+function Link({ data, page}) {
+
+  const handleClick = (item) => {
+    // Push an event to the dataLayer
+    window.dataLayer = window.dataLayer || [];
+    window.dataLayer.push({
+      event: 'button_click_'+page,
+      link: item.link,
+      name: item.name,
+      description: item.description,
+    });
+
+    // Open the link in a new tab
+    window.open(item.link, "_blank");
+  };
+
     return (
         <div className='card'>
       {data.map((item, index) => (
         <button
           className="link"
           key={index}
-          onClick={() => window.open(item.link, "_blank")}
+          onClick={() => handleClick(item)}
           >
           <div className='contents'
           >
