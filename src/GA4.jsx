dfe4fa22e86f7524ba1d48edf35a04687dd2e83c
Victorer <rainishere20@gmail.com>
2024-11-18 21:26:27 +0000

    auto push

--- src/GA4.jsx
+++ src/GA4.jsx
@@ -1,39 +1,44 @@
-import { useEffect, useLocation } from 'react';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import {
     doc,
     getDoc
   } from "firebase/firestore";
 
+import { db } from "./main.jsx";
+
 function GA4() {
 
-    const [documentData, setDocumentData] = useState(null);
 
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const code = searchParams.get('code');
   const source = searchParams.get('utm_source');
 
-  useEffect( async () => {
-    
-    if (code) {
+  useEffect(() => {
+    const fetchDocument = async () => {
+      if (code) {
         const docRef = doc(db, 'Links', code);
         const docSnap = await getDoc(docRef);
 
         if (docSnap.exists()) {
-            setDocumentData(docSnap.data());
-            if(source){
-                window.location.href = docSnap.data().redirectUrl;
-            }else{
-                window.location.href = docSnap.data().url;
-            }
+          if (source) {
+            window.location.href = docSnap.data().redirectUrl;
+          } else {
+            window.location.href = docSnap.data().url;
+          }
         } else {
-            console.log('No such document!');
+          console.log('No such document!');
+        //   window.location.href = 'https://www.investingwithrain.com';
         }
+      } else {
+        // Redirect to the external URL if no code is provided
+        window.location.href = 'https://www.investingwithrain.com';
       }
+    };
 
-    // Redirect to the external URL after the component mounts
-    window.location.href = 'https://www.investingwithrain.com';
-  }, []);
+    fetchDocument();
+  }, [code, source]);
 
   return null; // This component doesn't need to render anything
 }
