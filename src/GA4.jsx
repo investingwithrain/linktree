816dcd6c4ce8ac4c0f7a8a91c0afef85e737a3ef
The Coding Papa <48286934+thecodingpapa@users.noreply.github.com>
2025-01-27 16:53:17 +0000

    auto push

--- src/GA4.jsx
+++ src/GA4.jsx
@@ -1,26 +1,20 @@
-import { useEffect } from 'react';
-import { useLocation } from 'react-router-dom';
-import {
-    doc,
-    getDoc
-  } from "firebase/firestore";
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import { doc, getDoc } from "firebase/firestore";
 
 import { db } from "./main.jsx";
 
 function GA4() {
-
-
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
-  const code = searchParams.get('code');
+  const code = searchParams.get("code");
 
   useEffect(() => {
     const fetchDocument = async () => {
       if (code) {
-        const docRef = doc(db, 'Links', code);
+        const docRef = doc(db, "Links", code);
         const docSnap = await getDoc(docRef);
         const data = docSnap.data();
-
 
         if (window.gtag) {
           // Send the UTM parameters as a custom event to Google Analytics
@@ -38,14 +32,28 @@ function GA4() {
         }
 
         if (docSnap.exists()) {
-            window.location.href = data.redirectUrl;
+          // Construct the UTM query string
+          const utmParams = new URLSearchParams({
+            utm_source: data.utmSource || "(not set)",
+            utm_medium: data.utmMedium || "(not set)",
+            utm_campaign: data.utmCampaign || "(not set)",
+            utm_term: data.utmTerm || "(not set)",
+            utm_content: data.utmContent || "(not set)",
+          });
+
+          // Append the UTM parameters to the redirect URL
+          const redirectUrl = new URL(data.redirectUrl);
+          redirectUrl.search +=
+            (redirectUrl.search ? "&" : "") + utmParams.toString();
+
+          window.location.href = data.redirectUrl;
         } else {
-          console.log('No such document!');
-        //   window.location.href = 'https://www.investingwithrain.com';
+          console.log("No such document!");
+          //   window.location.href = 'https://www.investingwithrain.com';
         }
       } else {
         // Redirect to the external URL if no code is provided
-        window.location.href = 'https://www.investingwithrain.com';
+        window.location.href = "https://www.investingwithrain.com";
       }
     };
 
@@ -53,9 +61,10 @@ function GA4() {
   }, [code]);
 
   return (
-    <div style={{ backgroundColor: 'black', width: '100vw', height: '100vh' }}>
+    <div style={{ backgroundColor: "black", width: "100vw", height: "100vh" }}>
       {/* This component doesn't need to render anything else */}
     </div>
-  );}
+  );
+}
 
 export default GA4;
